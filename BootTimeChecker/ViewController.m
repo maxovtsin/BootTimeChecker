df14c5a54c1421fc1f70de4373bab21f371b893e
Max Ovtsin <maxovtsin@fb.com>
2020-01-09 13:27:08 +0000

    Initial commit

--- BootTimeChecker/ViewController.m
+++ BootTimeChecker/ViewController.m
@@ -7,8 +7,24 @@
 //
 
 #import "ViewController.h"
+#include <sys/sysctl.h>
+
+typedef struct timeval timeval_t;
+
+timeval_t kern_boottime() {
+  struct timeval boottime;
+  int mib[2] = {CTL_KERN, KERN_BOOTTIME};
+  size_t size = sizeof(boottime);
+  if (sysctl(mib, 2, &boottime, &size, NULL, 0) != -1 && boottime.tv_sec != 0) {
+    return boottime;
+  }
+  return (timeval_t){ .tv_sec = 0, .tv_usec = 0 };
+}
 
 @interface ViewController ()
+
+@property (nonatomic, weak) IBOutlet UILabel *labelBootTime;
+@property (nonatomic, weak) IBOutlet UILabel *labelBootTimePrecise;
 
 @end
 
@@ -16,7 +32,13 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  // Do any additional setup after loading the view.
+  
+  timeval_t bt = kern_boottime();
+  
+  NSDate* bootDate = [NSDate dateWithTimeIntervalSince1970:bt.tv_sec + bt.tv_usec / 1.e6];
+  self.labelBootTime.text = [NSString stringWithFormat:@"%@", bootDate];
+  
+  self.labelBootTimePrecise.text = [NSString stringWithFormat:@"Secs: %lld USecs: %lld", (long long)bt.tv_sec, (long long)bt.tv_usec];
 }
 
 
